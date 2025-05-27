adb9ead494408e4afaff6d6280815f49733f647a
dennisworld01 <140074666+dennisworld01@users.noreply.github.com>
2025-05-27 07:09:20 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const subscribers = Math.floor(Math.random() * 11) + 10;
-  const visitors = Math.floor(Math.random() * 1001) + 2000;
-  document.getElementById("subscribers").textContent = "Subscribers: " + subscribers;
-  document.getElementById("visitors").textContent = "Visitors Today: " + visitors;
-});
