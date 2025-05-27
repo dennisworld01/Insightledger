843280346ceda9f478893d7d197df43511715a79
dennisworld01 <140074666+dennisworld01@users.noreply.github.com>
2025-05-27 14:19:13 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-  const baseFollowers = 7429;
-  const newSubs = Math.floor(Math.random() * (20 - 10 + 1)) + 10;
-  const visitors = Math.floor(Math.random() * (3000 - 2000 + 1)) + 2000;
-
-  document.getElementById("Followers-count").innerText = "Followers: " + (baseFollowers + newSubs).toLocaleString();
-  document.getElementById("visitor-count").innerText = "Today's Visitors: " + visitors.toLocaleString();
-});
